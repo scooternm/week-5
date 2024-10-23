78973d5fd0fdcfc9d2087e03f6e5a4a8ad0c2582
Kay <mccray382@gmail.com>
2024-10-23 16:00:27 -0400

    k

---
+++ food_menu.js
@@ -0,0 +1,34 @@
+let menuObject = {
+drinkList : [
+"Water", "Tea", "Sweet Tea",
+"Coke", "Dr. Pepper", "Sprite"
+],
+entreeList : [
+"Hamburger w/ Fries",
+"Grilled Cheese w/ Tater Tots",
+"Grilled Chicken w/ Veggies",
+"Chicken Fried Steak w/ Mashed Potatoes",
+"Fried Shrimp w/ Coleslaw",
+"Veggie Plate"
+],
+dessertList: [
+"Cheesecake", "Chocolate Cake", "Snickerdoodle Cookie"
+]
+};
+
+// console.log(menuObject.drinkList)
+for(key in menuObject){
+    let list = menuObject[key];
+    console.log(key.toUppercase(), list.length);
+    let i = 0;
+    for(text of list){
+        console.log("\t\t", i, text);
+        i++;
+    }
+    // console.log(list[0])
+    // for(let i=0; i < list.length;){
+    //     const text = list[i];
+    //     console.log(i, text);
+    }
+
+}
